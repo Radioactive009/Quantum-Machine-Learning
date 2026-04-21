972c9a6789783065a7fd8994e5397ef793147a9f
Kislay Kumar <kislayreigns20092002@gmail.com>
2026-04-21 15:25:26 +0530

    initial commit

--- dataset.py
+++ dataset.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Normal data (tight cluster around [1,1])
-normal_data = np.random.normal(loc=1, scale=0.2, size=(50, 2))
+normal_data = np.random.normal(loc=[1, 1], scale=0.2, size=(50, 2))
 
-# Anomalies (opposite direction)
-anomalies = np.random.normal(loc=-1, scale=0.2, size=(10, 2))
+# Anomalies (orthogonal direction)
+anomalies = np.random.normal(loc=[1, -1], scale=0.2, size=(10, 2))
 
 # Combine for visualization
 all_data = np.vstack((normal_data, anomalies))
